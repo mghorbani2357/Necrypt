fd3c7154a4db15ae14522f5eb78aa9da54c65de9
ghorbani <m.ghorbani2357@gmail.com>
2021-02-03 10:19:33 +0330

    Added _version.py.

--- necrypt/__init__.py
+++ necrypt/__init__.py
@@ -3,3 +3,7 @@ __author__ = 'Mohsen Ghorbani'
 # from ._version import get_versions
 # __version__ = get_versions()['version']
 # del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
